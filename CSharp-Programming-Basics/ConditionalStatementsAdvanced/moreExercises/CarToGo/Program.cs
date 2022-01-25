46f77488ca8d55c20196a01822d8e818724d9225
ivana <iva1100b@gmail.com>
2022-01-25 08:43:28 +0200

    Added exercises

---
+++ CSharp-Programming-Basics/ConditionalStatementsAdvanced/moreExercises/CarToGo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CarToGo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string classType = "";
+            string carType = "";
+            double price = 0;
+
+            if (budget <= 100)
+            {
+                classType = "Economy class";
+
+                switch (season)
+                {
+                    case "Summer":
+                        carType = "Cabrio";
+                        price = budget * 0.35;
+                        break;
+                    case "Winter":
+                        carType = "Jeep";
+                        price = budget * 0.65;
+                        break;
+                }
+            }
+            else if (budget > 100 && budget <= 500)
+            {
+                classType = "Compact class";
+
+                switch (season)
+                {
+                    case "Summer":
+                        carType = "Cabrio";
+                        price = budget * 0.45;
+                        break;
+                    case "Winter":
+                        carType = "Jeep";
+                        price = budget * 0.80;
+                        break;
+                }
+            }
+            else
+            {
+                classType = "Luxury class";
+                carType = "Jeep";
+                price = budget * 0.90;
+            }
+
+            Console.WriteLine($"{classType}");
+            Console.WriteLine($"{carType} - {price:F2}");
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/ConditionalStatementsAdvanced/moreExercises/Vacation/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            string placeType = "";
+            double price = 0;
+
+            if (budget <= 1000)
+            {
+                placeType = "Camp";
+
+                switch (season)
+                {
+                    case "Summer":
+                        destination = "Alaska";
+                        price = budget * 0.65;
+                        break;
+                    case "Winter":
+                        destination = "Morocco";
+                        price = budget * 0.45;
+                        break;
+                }
+            }
+            else if (budget > 1000 && budget <= 3000)
+            {
+                placeType = "Hut";
+
+                switch (season)
+                {
+                    case "Summer":
+                        destination = "Alaska";
+                        price = budget * 0.80;
+                        break;
+                    case "Winter":
+                        destination = "Morocco";
+                        price = budget * 0.60;
+                        break;
+                }
+            }
+            else
+            {
+                placeType = "Hotel";
+                price = budget * 0.90;
+
+                switch (season)
+                {
+                    case "Summer":
+                        destination = "Alaska";
+                        break;
+                    case "Winter":
+                        destination = "Morocco";
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{destination} - {placeType} - {price:F2}");
+        }
+    }
+}
