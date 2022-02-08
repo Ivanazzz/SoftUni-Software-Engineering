563b26d15cbe90c8a12dc979a94c867ecb235e8f
ivana <iva1100b@gmail.com>
2022-02-08 09:08:29 +0200

    Add exercises

---
+++ CSharp-Programming-Basics/NestedLoops/lecture/Travelling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+
+                if (destination == "End")
+                {
+                    break;
+                }
+
+                double neededSum = double.Parse(Console.ReadLine());
+
+                while (true)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    neededSum -= money;
+
+                    if (neededSum <= 0)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
