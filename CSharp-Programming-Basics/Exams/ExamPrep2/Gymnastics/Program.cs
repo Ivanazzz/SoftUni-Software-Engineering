6b4e88ac2467f661de9c01d2e17f031edd1c1cae
ivana <iva1100b@gmail.com>
2022-02-10 13:06:52 +0200

    Add exercises

---
+++ CSharp-Programming-Basics/Exams/ExamPrep2/Gymnastics/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Gymnastics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string appliance = Console.ReadLine();
+
+            double difficulty = 0;
+            double performance = 0;
+
+            switch (country)
+            {
+                case "Russia":
+                    switch (appliance)
+                    {
+                        case "ribbon":
+                            difficulty = 9.100;
+                            performance = 9.400;
+                            break;
+                        case "hoop":
+                            difficulty = 9.300;
+                            performance = 9.800;
+                            break;
+                        case "rope":
+                            difficulty = 9.600;
+                            performance = 9.000;
+                            break;
+                    }
+                    break;
+                case "Bulgaria":
+                    switch (appliance)
+                    {
+                        case "ribbon":
+                            difficulty = 9.600;
+                            performance = 9.400;
+                            break;
+                        case "hoop":
+                            difficulty = 9.550;
+                            performance = 9.750;
+                            break;
+                        case "rope":
+                            difficulty = 9.500;
+                            performance = 9.400;
+                            break;
+                    }
+                    break;
+                case "Italy":
+                    switch (appliance)
+                    {
+                        case "ribbon":
+                            difficulty = 9.200;
+                            performance = 9.500;
+                            break;
+                        case "hoop":
+                            difficulty = 9.450;
+                            performance = 9.350;
+                            break;
+                        case "rope":
+                            difficulty = 9.700;
+                            performance = 9.150;
+                            break;
+                    }
+                    break;
+            }
+
+            double totalPoints = difficulty + performance;
+            double neededPoints = 20 - totalPoints;
+            double neededPointsInPercentage = (neededPoints / 20) * 100;
+
+            Console.WriteLine($"The team of {country} get {totalPoints:F3} on {appliance}.");
+            Console.WriteLine($"{neededPointsInPercentage:F2}%");
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exams/ExamPrep2/TennisEquipment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TennisEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tennisRacketPrice = double.Parse(Console.ReadLine());
+            int tennisRacketCount = int.Parse(Console.ReadLine());
+            int pairsOfSneakersCount = int.Parse(Console.ReadLine());
+
+            double pairsOfSneakersPrice = tennisRacketPrice / 6;
+            double tennisRacketTotalPrice = tennisRacketCount * tennisRacketPrice;
+            double pairsOfSneakersTotalPrice = pairsOfSneakersCount * pairsOfSneakersPrice;
+            double anotherEquipmentPrice = (tennisRacketTotalPrice + pairsOfSneakersTotalPrice) * 0.2;
+            double totalPrice = tennisRacketTotalPrice + pairsOfSneakersTotalPrice + anotherEquipmentPrice;
+            double sponsorsPrice = totalPrice * 7 / 8;
+            double playersPrice = totalPrice / 8;
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(playersPrice)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(sponsorsPrice)}");
+        }
+    }
+}
