ec5b137c8c231c38a0aed6bbbdf01703a5fcd865
ivana <iva1100b@gmail.com>
2022-02-13 11:45:04 +0200

    Add exercises

---
+++ CSharp-Programming-Basics/NestedLoops/lab/CinemaTickets/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentTicketsCount = 0;
+            int standardTicketsCount = 0;
+            int kidTicketsCount = 0;
+
+            string moviesName = Console.ReadLine();
+
+            while (moviesName != "Finish")
+            {
+                int totalPlaces = int.Parse(Console.ReadLine());
+                int occupiedPlaces = 0;
+
+                while (true)
+                {
+                    string ticketType = Console.ReadLine();
+                    if (ticketType == "End")
+                    {
+                        break;
+                    }
+                    occupiedPlaces++;
+                    switch (ticketType)
+                    {
+                        case "student":
+                            studentTicketsCount++;
+                            break;
+                        case "standard":
+                            standardTicketsCount++;
+                            break;
+                        case "kid":
+                            kidTicketsCount++;
+                            break;
+                    }
+
+                    if (occupiedPlaces == totalPlaces)
+                    {
+                        break;
+                    }
+                }
+
+                double occupiedPlacesPercentage = occupiedPlaces * 1.00 / totalPlaces * 100;
+                Console.WriteLine($"{moviesName} - {occupiedPlacesPercentage:F2}% full.");
+
+                moviesName = Console.ReadLine();
+            }
+
+            int totalTickets = studentTicketsCount + standardTicketsCount + kidTicketsCount;
+            double studentTicketsPercentage = studentTicketsCount * 1.00 / totalTickets * 100;
+            double standardTicketsPercentage = standardTicketsCount * 1.00 / totalTickets * 100;
+            double kidTicketsPercentage = kidTicketsCount * 1.00 / totalTickets * 100;
+
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{studentTicketsPercentage:F2}% student tickets.");
+            Console.WriteLine($"{standardTicketsPercentage:F2}% standard tickets.");
+            Console.WriteLine($"{kidTicketsPercentage:F2}% kids tickets.");
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/NestedLoops/lab/EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EqualSumsEvenOddPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int number = firstNum; number <= secondNum; number++)
+            {
+                int evenDigitsSum = 0;
+                int oddDigitsSum = 0;
+
+                string currentNum = number.ToString();
+
+                for (int digit = 0; digit < currentNum.Length; digit++)
+                {
+                    if (digit % 2 != 0)
+                    {
+                        evenDigitsSum += currentNum[digit];
+                    }
+                    else
+                    {
+                        oddDigitsSum += currentNum[digit];
+                    }
+                }
+
+                if (evenDigitsSum == oddDigitsSum)
+                {
+                    Console.Write(currentNum + " ");
+                }
+            }
+        }
+    }
+}
