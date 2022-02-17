378297fb6f129b70cfafd3a9a81accf7030e8b40
ivana <iva1100b@gmail.com>
2022-02-17 10:35:43 +0200

    Add exercises

---
+++ CSharp-Programming-Basics/Exams/ExamPrep5/Club/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Club
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double wantedProfit = double.Parse(Console.ReadLine());
+
+            double pricePerCocktail = 0;
+            double totalIncome = 0;
+
+            while (true)
+            {
+                string cocktail = Console.ReadLine();
+                if (cocktail == "Party!")
+                {
+                    Console.WriteLine($"We need {(wantedProfit - totalIncome):F2} leva more.");
+                    break;
+                }
+                int numberOfCocktails = int.Parse(Console.ReadLine());
+                pricePerCocktail = cocktail.Length * numberOfCocktails;
+                if ((pricePerCocktail % 10) % 2 != 0)
+                {
+                    pricePerCocktail -= pricePerCocktail * 0.25;
+                }
+                totalIncome += pricePerCocktail;
+                if (totalIncome >= wantedProfit)
+                {
+                    Console.WriteLine("Target acquired.");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Club income - {totalIncome:F2} leva.");
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exams/ExamPrep5/CoffeeMachine/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CoffeeMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string sugar = Console.ReadLine();
+            int drinksCount = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            switch (drink)
+            {
+                case "Espresso":
+                    switch (sugar)
+                    {
+                        case "Without":
+                            price = 0.90;
+                            break;
+                        case "Normal":
+                            price = 1.00;
+                            break;
+                        case "Extra":
+                            price = 1.20;
+                            break;
+                    }    
+                    break;
+                case "Cappuccino":
+                    switch (sugar)
+                    {
+                        case "Without":
+                            price = 1.00;
+                            break;
+                        case "Normal":
+                            price = 1.20;
+                            break;
+                        case "Extra":
+                            price = 1.60;
+                            break;
+                    }
+                    break;
+                case "Tea":
+                    switch (sugar)
+                    {
+                        case "Without":
+                            price = 0.50;
+                            break;
+                        case "Normal":
+                            price = 0.60;
+                            break;
+                        case "Extra":
+                            price = 0.70;
+                            break;
+                    }
+                    break;
+            }
+
+            double totalPrice = drinksCount * price;
+
+            if (sugar == "Without")
+            {
+                totalPrice -= totalPrice * 0.35;
+            }
+            if (drink == "Espresso" && drinksCount >= 5)
+            {
+                totalPrice -= totalPrice * 0.25;
+            }
+            if (totalPrice > 15)
+            {
+                totalPrice -= totalPrice * 0.2;
+            }
+
+            Console.WriteLine($"You bought {drinksCount} cups of {drink} for {totalPrice:F2} lv.");
+        }
+    }
+}
