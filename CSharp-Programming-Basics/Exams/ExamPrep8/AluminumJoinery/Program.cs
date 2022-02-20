be27b3cd455e28bb4435003856b831dd82e6ee4b
ivana <iva1100b@gmail.com>
2022-02-20 10:00:12 +0200

    Add exercises

---
+++ CSharp-Programming-Basics/Exams/ExamPrep8/AluminumJoinery/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace AluminumJoinery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int joineryCount = int.Parse(Console.ReadLine());
+            string joineryType = Console.ReadLine();
+            string delivery = Console.ReadLine();
+
+            if (joineryCount < 10)
+            {
+                Console.WriteLine("Invalid order");
+                return;
+            }
+
+            double price = 0;
+
+            switch (joineryType)
+            {
+                case "90X130":
+                    price = joineryCount * 110;
+                    if (joineryCount > 60)
+                    {
+                        price -= price * 0.08;
+                    }
+                    else if (joineryCount > 30)
+                    {
+                        price -= price * 0.05;
+                    }
+                    break;
+                case "100X150":
+                    price = joineryCount * 140;
+                    if (joineryCount > 80)
+                    {
+                        price -= price * 0.1;
+                    }
+                    else if (joineryCount > 40)
+                    {
+                        price -= price * 0.06;
+                    }
+                    break;
+                case "130X180":
+                    price = joineryCount * 190;
+                    if (joineryCount > 50)
+                    {
+                        price -= price * 0.12;
+                    }
+                    else if (joineryCount > 20)
+                    {
+                        price -= price * 0.07;
+                    }
+                    break;
+                case "200X300":
+                    price = joineryCount * 250;
+                    if (joineryCount > 50)
+                    {
+                        price -= price * 0.14;
+                    }
+                    else if (joineryCount > 25)
+                    {
+                        price -= price * 0.09;
+                    }
+                    break;
+            }
+
+            if (delivery == "With delivery")
+            {
+                price += 60;
+            }
+
+            if (joineryCount > 99)
+            {
+                price -= price * 0.04;
+            }
+
+            Console.WriteLine($"{price:F2} BGN");
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/Exams/ExamPrep8/BestPlayer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BestPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bestPlayersName = "";
+            int bestPlayersGoals = 0;
+            bool isValid = false;
+
+            string player = Console.ReadLine();
+            while (player != "END")
+            {
+                int goals = int.Parse(Console.ReadLine());
+
+                if (goals > bestPlayersGoals)
+                {
+                    bestPlayersGoals = goals;
+                    bestPlayersName = player;
+                    if (goals >= 10)
+                    {
+                        isValid = true;
+                        break;
+                    }
+                    if (goals >= 3)
+                    {
+                        isValid = true;
+                    }
+                }
+                player = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{bestPlayersName} is the best player!");
+            if (isValid)
+            {
+                Console.WriteLine($"He has scored {bestPlayersGoals} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestPlayersGoals} goals.");
+            }
+        }
+    }
+}
