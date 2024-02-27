5e2fc5d1fb7f83a0922bdeb7b954e8843a557aa1
Ivana <iva1100b@gmail.com>
2024-02-27 08:17:40 +0200

    ServiceCollectionExtensions added to simplify Program.cs

---
+++ CSharp-Web/CSharp-Web-2024/ASP.NET-Advanced/Workshop/HouseRentingSystem/HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        // For extension methods the first parameter is the thing we extend
+        // With builder.Services.AddApplicationServices() we pass the Services like first parameter
+        // We retrun services only if we want in the Program.cs to make method chaining
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services
+                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
---
+++ CSharp-Web/CSharp-Web-2024/ASP.NET-Advanced/Workshop/HouseRentingSystem/HouseRentingSystem/Program.cs
@@ -0,0 +1,41 @@
+namespace HouseRentingSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddApplicationDbContext(builder.Configuration);
+            builder.Services.AddApplicationIdentity(builder.Configuration);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddApplicationServices();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapDefaultControllerRoute();
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
